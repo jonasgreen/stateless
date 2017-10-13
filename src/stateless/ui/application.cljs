1ed0d238cc681906b362c2923c80a50a590e6a88
Jonas Green <jg@skm.dk>
2017-10-13 11:32:54 +0200

    Reintroducing cljs

--- src/stateless/ui/application.cljs
+++ src/stateless/ui/application.cljs
@@ -3,7 +3,7 @@
             [stateless.ui.styles :as s]
             [stateless.state :as state]
             [stateless.ui.contact-content :as contact-tab]
-            [stateless.ui.about_tab :as about-tab]))
+            [stateless.ui.about-content :as about-tab]))
 
 ;------------
 ; menu-items
@@ -30,43 +30,79 @@
 
 
 (defn menu []
-  [:div {:style {:padding "0 50px 40px"
-                 :display :flex
+  [:div {:style {:padding         "0 50px 40px"
+                 :display         :flex
                  :justify-content :space-between
-                 :font-size 20
-                 :background :red
-                 :align-items :flex-end
-                 :flex-wrap :wrap}}])
+                 :font-size       20
+                 :background      :red
+                 :align-items     :flex-end
+                 :flex-wrap       :wrap}}])
 
 
 (defn content []
   [:div {:style {:padding         50
                  :flex-grow       1
-                 :background :blue
+                 :background      :blue
                  :display         :flex
                  :justify-content :center
                  :align-items     :center}}
-
 
    ]
   )
 
 
+
+
 (defn render [_]
   (let [local-state (r/atom {})]
     (fn [state]
-      (println "RENDER APP")
-      [:div {:style {:height "100vh"
-                     :width "100vw"
-                     :display        :flex
-                     :flex-direction :column}}
-       [content]
-       [menu]]
+      [:div {:style {:height          :100vh
+                     :width           :100vw
+                     :display         :flex
+                     :flex-direction  :column
+                     :align-items     :center
+                     :justify-content :center}}
+       [:img {:src   "img/the-ocean.png"
+              :style {:height   :100vh
+                      :width    :100vw
+                      :opacity  0.17
+                      :position :fixed
+                      :top      0
+                      :left     0
+                      :z-index  -1}}]
 
-      #_(fn [ctx]
-          [:div {:style s/tournament-page-style}
-           [menu-panel m-items selector]
-           ;content
-           [teams/render ctx]
-           #_(when-let [{:keys [render name]} (sel/selected selector)]
-               [render (context/sub-ui-ctx old-ctx [(keyword (str (clojure.string/lower-case name) "-tab"))]) ctx])]))))
+       ;top
+       [:div {:style {:flex-grow 2}}]
+
+       ;bottom
+       [:div {:style {:flex-grow       1
+                      :display         :flex
+                      :flex-direction  :column
+                      :justify-content :center
+                      :align-items     :center}}
+
+        [:div
+         ;title
+         [:div {:style {:font-size       24
+                        :color           "rgba(174, 182, 187, 1)"
+                        :display         :flex
+                        :line-height     1.5                
+                        :justify-content :space-between}}
+          [:span "S"] [:span "T"] [:span "A"] [:span "T"] [:span "E"] [:span "L"] [:span "E"] [:span "S"] [:span "S"]]
+
+         ;sub-title
+         [:div {:style {:font-size       14
+                        :color           "rgba(174, 182, 187, 1)"
+                        :display         :flex
+                        :justify-content :space-between
+                        :padding-bottom  30
+                        :padding-left    1}} "Software Development"]]
+
+
+        ;contact info
+        [:div {:style {:display         :flex
+                       :justify-content :center
+                       :align-items     :center}}
+         [:div {:style {:font-size 14
+                        :color     "rgba(73, 78, 84, 1)"}}
+          "Jonas Green | jg@stateless.dk | +45 2149 7961"]]]])))
