633d8c2ed015548aa01adac647819c1f703b7804
Jonas Green <jg@skm.dk>
2017-10-16 15:30:32 +0200

    asdf

--- src/stateless/ui/application.cljs
+++ src/stateless/ui/application.cljs
@@ -47,10 +47,7 @@
                  :background      :blue
                  :display         :flex
                  :justify-content :center
-                 :align-items     :center}}
-
-   ]
-  )
+                 :align-items     :center}}])
 
 
 
@@ -75,7 +72,16 @@
                                 :z-index  -1}}]
 
                  ;top
-                 [:div {:style {:flex-grow 2}}]
+                 [:div {:style {:flex-grow 2 :width :100%}}
+
+                  [:div {:style {:display :flex :padding 80 :padding-top 60 :font-size 14 :color "rgba(73, 78, 84, 1)"}}
+                   [:div {:style {:display :flex :justify-content :space-between :align-items :center}}
+                    [:span {:style {:padding 2 :margin-right 40 :color "rgba(174, 182, 187, 1)" :border-bottom "1px solid rgba(174, 182, 187, 1)"}} "ABOUT"]
+                    [:span {:style {:margin-right 40}} "CONTACT"] [:span "BAR"]]
+
+                   ]
+
+                  ]
 
                  ;bottom
                  [:div {:style {:flex-grow       1
