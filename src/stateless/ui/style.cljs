f2aeb843a71c2dc5c49c0befea054938dfef0a85
Jonas Green <jg@skm.dk>
2017-10-26 15:18:27 +0200

    animation stille needs deletions

--- src/stateless/ui/style.cljs
+++ src/stateless/ui/style.cljs
@@ -9,10 +9,32 @@
 (def z-index-footer 500)
 (def z-index-tooling 1000)
 
+(def none-pixel-styles (set [:column-count
+                             :fill-opacity
+                             :flex
+                             :flex-grow
+                             :flex-shrink
+                             :font-weight
+                             :line-clamp
+                             :line-height
+                             :opacity
+                             :order
+                             :orphans
+                             :widows
+                             :z-index
+                             :zoom]))
+
+(defn convert-value [k v]
+  (cond
+    (and (int? v) (not (contains? none-pixel-styles k))) (str v "px")
+    (keyword? v) (name v)
+    :else v))
+
+(defn convert-key [k] (name k))
 
 (defn- style [style style-map]
   (doseq [[k v] style-map]
-    (aset style (name k) v)))
+    (aset style (convert-key k) (convert-value k v))))
 
 (defn style-current-target [event style-map]
   (style (.. event -currentTarget -style) style-map))
@@ -25,7 +47,6 @@
 
 (defn- prepend-into-opts [opts & styles]
   (merge-with merge {:style (apply merge styles)} opts))
-
 
 (defn style-node [node style-map]
   (style (aget node "style") style-map))
--- src/stateless/ui/transition_group.cljs
+++ src/stateless/ui/transition_group.cljs
@@ -8,15 +8,60 @@
 (defn items-by-id-map [items]
   (reduce (fn [col {:keys [id] :as child}] (assoc col id child)) {} items))
 
+
+(defn remove-deleted [items]
+  (filter :_tg_deleted items))
+
+(defn change-model [old-items new-items]
+  #_"
+  Deleted Items are marked :_tg_deleted, but left in items to ensure transition before the are later removed.
+
+  Before investigating what items are added and deleted - _tg_deleted items are temporary removed to avoid interference.
+
+  Deleted items are added to their original position in items.
+
+
+  "
+
+  (let [old-items-by-id (items-by-id-map old-items)
+        new-items-by-id (items-by-id-map new-items)
+        old-with-deleted-items-by-id (items-by-id-map (filter #(not (:_tg_deleted %)) old-items))
+        old-deleted-items-by-id (items-by-id-map (filter :_tg_deleted old-items))
+
+
+        old-ids (set (keys old-items-by-id))
+        new-ids (set (keys new-items-by-id))
+        old-without-deleted-ids (set (keys old-deleted-items-by-id))
+
+        deleted-ids (c-set/difference old-ids new-ids)
+        deleted-items (-> (select-keys old-items-by-id deleted-ids) vals)
+
+        added-ids (c-set/difference new-ids old-ids)
+        added-items (-> (select-keys new-items-by-id added-ids) vals)]
+
+
+
+    ;
+
+    {:items         nil
+     :added-items   nil
+     :deleted-items nil}))
+
+(defn style-it [id s]
+  (if-let [node (dom/getElement (str id))]
+    (style/style-node node s)
+    (println "No dom-node for aaaaID:" id)))
+
 (defn transitions [life-cycle transition-styles children]
-  (when-let [get-style (get transition-styles life-cycle)]
-    (let [do-style (fn [c]
-                     (when-let [node (dom/getElement (str (:id c)))]
-                       (style/style-node node (get-style c))))]
-      (mapv do-style children))))
+  (if-let [get-style (get transition-styles life-cycle)]
+    (let [do-style (fn [c] (if-let [node (dom/getElement (str (:id c)))]
+                             (style/style-node node (get-style c))
+                             (println "No dom-node found for id: " (:id c))))]
+      (mapv do-style children))
+    (println "No transition-style found for life-cycle:" life-cycle)))
 
 (defn tg [{:keys [children-data child-factory enter-timeout leave-timeout transition-styles] :as input}]
-  (let [children (r/atom (mapv #(assoc % :transition-state :will-appear) children-data))
+  (let [children (r/atom children-data)
         ts (transition-styles enter-timeout leave-timeout)]
 
     (r/create-class
@@ -25,60 +70,93 @@
                                        (r/next-tick #(transitions :did-appear ts @children)))
 
        :component-will-receive-props (fn [this new-argv]
-                                       (let [old-items-by-id (items-by-id-map @children)
-                                             new-items-by-id (items-by-id-map (second new-argv))
+                                       (let [new-items (-> new-argv second :children-data)
+                                             old-items-by-id (items-by-id-map @children)
+                                             new-items-by-id (items-by-id-map new-items)
 
-                                             old-ids (keys old-items-by-id)
-                                             new-ids (keys new-items-by-id)
+                                             old-ids (set (keys old-items-by-id))
+                                             new-ids (set (keys new-items-by-id))
 
-                                             ;deleted-ids (c-set/difference old-ids new-ids)
+                                             deleted-ids (c-set/difference old-ids new-ids)
+                                             deleted-items (-> (select-keys old-items-by-id deleted-ids) vals)
+
                                              added-ids (c-set/difference new-ids old-ids)
-                                             added-items (-> (select-keys new-items-by-id added-ids)
-                                                             vals)]
+                                             ]
 
-                                         (reset! children (second new-argv))
+
+                                         ;ADDED ITEMS HAVE TO BE REMOVED FROM DELETED ITEMS
+
+
+                                         (reset! children new-items)))
+
+       :component-did-update         (fn [this old-argv]
+                                       (let [old-items-by-id (items-by-id-map (-> old-argv second :children-data))
+                                             new-items-by-id (items-by-id-map @children)
+
+                                             old-ids (set (keys old-items-by-id))
+                                             new-ids (set (keys new-items-by-id))
+
+                                             deleted-ids (c-set/difference old-ids new-ids)
+                                             deleted-items (-> (select-keys old-items-by-id deleted-ids) vals)
+
+                                             added-ids (c-set/difference new-ids old-ids)
+                                             added-items (-> (select-keys new-items-by-id added-ids) vals)]
+
+                                         (println "added items" added-items)
 
                                          (transitions :will-enter ts added-items)
-                                         (r/next-tick #(transitions :did-enter ts added-items))
+                                         (r/next-tick #(transitions :did-enter ts added-items))))
 
-                                         (println "new-argv" (second new-argv))
-                                         (reset! children (:children-data (second new-argv)))))
+
        :render                       (fn [this]
                                        [:div {:style {:position :relative}}
                                         (map (fn [{:keys [id] :as c}] ^{:key (str id)} [child-factory c]) @children)])})))
 
 
-(defn child [{:keys [id label top left]}]
-  [:div {:id (str id) :style {:display         :flex
-                              :align-items     :center
-                              :justify-content :center
-                              :position        :absolute
-                              :top             top
-                              :left            left
-                              :background      :red
-                              :transition      (str "left " (rand-int 10) "s linear, " "top " (rand-int 10) "s linear")
-                              :font-size       10 :border-radius 40 :height 40 :width 40}} id])
+(defn child [_]
+  (r/create-class
+    {:component-will-receive-props (fn [this old-argv]
+                                     (let [{:keys [id label top left]} (r/props this)]
+                                       (style-it id {:transition (str "left " left "ms linear, top "left "ms linear")})))
+
+     :render                       (fn [this]
+                                     (let [{:keys [id label top left]} (r/props this)]
+                                       [:div {:id    (str id)
+                                              :style {:display         :flex
+                                                      :align-items     :center
+                                                      :justify-content :center
+                                                      :position        :absolute
+                                                      :top             top
+                                                      :left            left
+                                                      :background      :red
+                                                      :font-size       10 :border-radius 40 :height 40 :width 40}} id]))}))
 
 (defn transition-styles [enter-timeout leave-timeout]
   {:will-appear (fn [child-data] {:opacity 0})
    :did-appear  (fn [child-data] {:opacity    1
-                                  :transition "opacity 1s linear"})
-   :will-enter  (fn [child-data])
-   :did-enter   (fn [child-data])
+                                  :transition "opacity 1s linear, left 1s linear, top 1s linear"})
+   :will-enter  (fn [child-data] {:opacity    0
+                                  :left       0
+                                  :background :blue})
+   :did-enter   (fn [child-data] {:opacity    1
+                                  :left       (:left child-data)
+                                  :background :red
+                                  :transition "opacity 1s linear, left 1s linear, top 1s linear, background 1s ease-in"})
    :will-leave  (fn [child-data])
    :did-leave   (fn [child-data])})
 
 (defn create-model [label]
-  {:id (gensym) :label label :left (rand-int 200) :top (rand-int 200)}
+  {:id (gensym) :label label :left 500 :top 100}
   )
 
 (defn render [_]
-  (let [children (r/atom [(create-model "A") (create-model "B") (create-model "C")])]
+  (let [children (r/atom [{:id (gensym) :label "A" :left 300 :top 100} {:id (gensym) :label "B" :left 500 :top 100}])]
     (fn []
       [:div
        [:div {:on-click (fn []
-                          (swap! children conj (create-model "!"))
-                          (swap! children #(mapv (fn [cs] (assoc cs :left (rand-int 200) :top (rand-int 200))) %))
+
+                          (swap! children #(mapv (fn [cs] (assoc cs :left (rand-int 1000) :top (rand-int 200))) %))
+                          (swap! children conj (create-model "B"))
 
                           )
               :style    {:border "1px solid black"}} "BUTTON"]
